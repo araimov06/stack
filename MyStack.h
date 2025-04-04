2b8c6a666721d781411c80840e60e2c64c614d20
araimov06 <raimovadhamjon@gmail.com>
2025-04-04 23:43:13 +0500

    Update MyStack.h

--- MyStack.h
+++ MyStack.h
@@ -1,96 +1,55 @@
-#ifndef MYSTACK_H
-#define MYSTACK_H
+#ifndef MyStack_h
+#define MyStack_h
 
-#include <iostream>
+template <typename stack>
+class MyStack{
+    private:
+        class MyListNode{
+            private:
+                stack value;
+                MyListNode* next;
 
-template <typename T>
-class MyStack {
-private:
-    struct Node {
-        T data;
-        Node* next;
-        Node(T value, Node* nextNode = nullptr) : data(value), next(nextNode) {}
-    };
+            public:
+                MyListNode(stack value){this -> value = value; this -> next = nullptr;};
+                stack getValue() const {return value;};
+                void setNext(MyListNode* nextNode) {next = nextNode;};
+                MyListNode* getNext() const {return next;};
+        };
+        MyListNode* head;
+    
+    public:
+        MyStack(){head = nullptr;}
+        ~MyStack(){
+            while(!empty()){
+                pop();
+            }
+        }
+        
+        bool empty() const {
+            return head == nullptr;
+        }
 
-    Node* top;
-    int size;
+        stack top() const{
+            if (empty()) {
+                throw std::logic_error("Stack is empty\n");
+            }
+            return head->getValue();
+        }
 
-public:
-    MyStack();
-    ~MyStack();
+        void pop() {
+            if (empty()) {
+                throw std::logic_error("Stack is empty\n");
+            }
+            MyListNode* temp = head;
+            head = head->getNext();
+            delete temp;
+        }
 
-    void push(T value);
-    void pop();
-    T peek() const;
-    bool isEmpty() const;
-    int getSize() const;
-    void printStack() const;
+        void push(stack value) {
+            MyListNode* newNode = new MyListNode(value);
+            newNode->setNext(head);
+            head = newNode;
+        }
 };
 
-// Constructor
-template <typename T>
-MyStack<T>::MyStack() : top(nullptr), size(0) {}
-
-// Destructor
-template <typename T>
-MyStack<T>::~MyStack() {
-    while (!isEmpty()) {
-        pop();
-    }
-}
-
-// Push element onto stack
-template <typename T>
-void MyStack<T>::push(T value) {
-    Node* newNode = new Node(value, top);
-    top = newNode;
-    size++;
-}
-
-// Pop element from stack
-template <typename T>
-void MyStack<T>::pop() {
-    if (isEmpty()) {
-        std::cerr << "Stack underflow: Cannot pop from an empty stack.\n";
-        return;
-    }
-    Node* temp = top;
-    top = top->next;
-    delete temp;
-    size--;
-}
-
-// Peek top element
-template <typename T>
-T MyStack<T>::peek() const {
-    if (isEmpty()) {
-        throw std::runtime_error("Stack is empty. Cannot peek.");
-    }
-    return top->data;
-}
-
-// Check if stack is empty
-template <typename T>
-bool MyStack<T>::isEmpty() const {
-    return top == nullptr;
-}
-
-// Get size of stack
-template <typename T>
-int MyStack<T>::getSize() const {
-    return size;
-}
-
-// Print stack elements
-template <typename T>
-void MyStack<T>::printStack() const {
-    Node* current = top;
-    std::cout << "Stack (top to bottom): ";
-    while (current) {
-        std::cout << current->data << " ";
-        current = current->next;
-    }
-    std::cout << std::endl;
-}
-
-#endif // MYSTACK_H
+#endif
